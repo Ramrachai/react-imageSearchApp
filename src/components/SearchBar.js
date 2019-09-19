c7ed97c1d101b698a0614206fcfc8565f32987b7
Ramrachai <ramrachaim@gmail.com>
2019-09-19 21:47:46 +0600

    add: app & searchbar components

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ""
+    };
+  }
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
-            <labe> Image Search</labe>
-            <input type="text" />
+            <label> Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => {
+                this.setState({ term: e.target.value.toUpperCase() });
+              }}
+            />
           </div>
         </form>
       </div>
