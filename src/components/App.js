8dc18aa870fa5364354b92be8bac3e34af975e68
Ramrachai <ramrachaim@gmail.com>
2019-09-19 17:33:26 +0600

    add: uploading files

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
+export default class app extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
