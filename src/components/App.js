69d7bc2b4e5d050548c75c80b80186b998ae39e2
Ramrachai <ramrachaim@gmail.com>
2019-09-20 17:16:51 +0600

    add: unsplash api integrated

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,49 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
+import Axios from "axios";
+import { async } from "q";
+
 export default class app extends Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  constructor(props) {
+    super(props);
+    this.state = {
+      img: []
+    };
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
   }
+  // onSearchSubmit(term) {
+  //   Axios.get("https://api.unsplash.com/search/photos", {
+  //     params: { query: term, per_page: 20 },
+  //     headers: {
+  //       Authorization:
+  //         "Client-ID 509950d3625ce9f46b51f3ba6956f4646aace89ea98e6b38f2a46c8565f5ded5"
+  //     }
+  //   }).then(res => {
+  //     const imgData = res.data.results[3].urls.thumb;
+  //     console.log(imgData);
+  //     this.setState({ img: imgData });
+  //   });
+  // }
+  onSearchSubmit = async term => {
+    const imgResponse = await Axios.get(
+      "https://api.unsplash.com/search/photos",
+      {
+        params: { query: term, per_page: 20 },
+        headers: {
+          Authorization:
+            "Client-ID 509950d3625ce9f46b51f3ba6956f4646aace89ea98e6b38f2a46c8565f5ded5"
+        }
+      }
+    );
+    this.setState({ img: imgResponse.data.results });
+    console.log(this.state.img);
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "20px" }}>
         <SearchBar handleSubmit={this.onSearchSubmit} />
+        <p> Total images found: {this.state.img.length} </p>
       </div>
     );
   }
