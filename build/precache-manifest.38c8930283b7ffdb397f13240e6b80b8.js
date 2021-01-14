674bfef7a708333591bd3c267c582582bcbf3a36
Ramrachai <ramrachaim@gmail.com>
2021-01-14 15:04:42 +0600

    added build folder

---
+++ build/precache-manifest.38c8930283b7ffdb397f13240e6b80b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92efcb77f171f24a64e52944ee3ccc5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8bcdac62c7b36735e7e9",
+    "url": "./static/css/main.d06ea801.chunk.css"
+  },
+  {
+    "revision": "51d3b7d181e1ab3881eb",
+    "url": "./static/js/2.78e708af.chunk.js"
+  },
+  {
+    "revision": "8bcdac62c7b36735e7e9",
+    "url": "./static/js/main.e3a391d6.chunk.js"
+  },
+  {
+    "revision": "f069f54ab73349815aca",
+    "url": "./static/js/runtime~main.23196797.js"
+  }
+]);
